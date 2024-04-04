4cafa774d797cc641f767b71f03a3db18abb3dfc
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-04-04 12:32:19 +0530

    array implementaion

---
+++ array/implementation/arrayPass&Return.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// passing array to Function
+// WAY - 1
+void myFunction(int arr[]){
+}
+
+// WAY - 2
+void myFunction(int arr[3]){
+}
+
+
+int main(){
+    int array[] = {1,2,3};
+    myFunction(array);
+}
---
+++ array/implementation/arrayimp.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// int x{1} is same int x = 1;
+// same as array 
+// int array[]{1,2,3,4}
+
+    // array declaration 
+    int n;
+    int arr[n];
+    int arr0[8];
+
+// array initilization
+    int arr1[3] = {1,2,3};
+    int arr2[] = { 1,2,3,4};
+    int arr3[5] = {1,2}; //[1,2,0,0,0];
+    int arr4[5] = {0}; //Trick [0,0,0,0,0] only use for 0
+    // int arr5[1] = {1,2,3,4};// compiler give you error
+
+
+}
