cff2a99973a21f83b22f04b987a5f924277ba2a1
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-07-21 17:28:50 +0530

    pattern

--- 21-27_PATTERN/p1.cpp
+++ 21-27_PATTERN/p1.cpp
@@ -19,6 +19,15 @@ void print2(int n){
     }
 }
 
+void print3(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << j ;
+        }
+        cout << endl;
+    }
+}
+
 
 int main(){
     int t;
@@ -26,6 +35,6 @@ int main(){
 for(int i = 0; i < t; i++){
     int n;
     cin >> n;
-print2(n);
+print3(n);
 }
 }
