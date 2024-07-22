276e667fec4f79d17e703fa123372ffeac13ad14
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-07-22 20:12:34 +0530

    pattern

--- 21-27_PATTERN/p1.cpp
+++ 21-27_PATTERN/p1.cpp
@@ -74,6 +74,38 @@ void print7(int n){
     }
 }
 
+void print8(int n){
+    for(int i = 0; i < n; i++){
+        // space
+        for(int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        //stars
+         for(int j = 0; j < 2 * n - (2*i+1); j++){
+             cout << "*";
+         }
+        // space
+         for(int j = 0; j < i; j++){
+             cout << " ";
+         }
+        cout << endl;
+    }
+}
+
+void print10(int n){
+    for(int i = 1; i <= 2 * n -1 ; i++){
+        int stars = i;
+        if(i > n){
+            stars = 2 * n - i;
+        }
+        for(int j = 1; j<= stars; j++){
+            cout << " * ";
+        }
+     
+        cout << endl;
+    }
+}
 
 int main(){
     int t;
@@ -81,6 +113,8 @@ int main(){
 for(int i = 0; i < t; i++){
     int n;
     cin >> n;
-print7(n);
+    print10(n);
+   
+
 }
 }
