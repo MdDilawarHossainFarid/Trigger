c53ddd282f05300884ac6a2e1d2698a665567339
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-07-22 20:56:06 +0530

    pattern

--- 21-27_PATTERN/p1.cpp
+++ 21-27_PATTERN/p1.cpp
@@ -107,13 +107,49 @@ void print10(int n){
     }
 }
 
+void print11(int n){
+   int start = 1;
+   for(int i = 0; i < n; i++){
+       if(i % 2 == 0){
+           start = 1;
+       } else {
+           start = 0;
+       }
+   for(int j = 0; j <= i; j++){
+       cout << start;
+       start = 1 - start;
+   }
+     
+        cout << endl;
+    }
+}
+
+void print12(int n){
+    int space = 2 * (n -1);
+    for(int i = 1; i <= n; i++){
+    // numbers
+    for(int j = 1; j <= i; j++){
+        cout << j;
+    }
+    // space
+    for(int j = 1; j <= space; j++){
+        cout << " ";
+    }
+    // numbers
+    for(int j = i; j >= 1; j--){
+        cout << j;
+    }
+cout << endl;
+space -= 2;
+    }
+}
 int main(){
     int t;
     cin >> t;
 for(int i = 0; i < t; i++){
     int n;
     cin >> n;
-    print10(n);
+    print12(n);
    
 
 }
