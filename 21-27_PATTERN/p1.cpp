1532344df1de1336a833eecc8194123a171cc4e2
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2024-07-23 23:04:22 +0530

    pattern

--- 21-27_PATTERN/p1.cpp
+++ 21-27_PATTERN/p1.cpp
@@ -204,6 +204,34 @@ void print19(int n){
 
 }
 
+void print20(int n){
+    int spaces = 2*n - 2;
+    for(int i = 1; i <= 2*n - 1; i++){
+        int stars = i;
+        if( i > n){
+            stars = 2*n - i;
+        }
+        // stars
+        for(int j = 1; j <= stars; j++){
+            cout << "*";
+        }
+        //spaces
+        for(int j = 1; j <= spaces; j++){
+            cout << " ";
+        }
+            // stars
+        for(int j = 1; j <= stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+        if( i < n){
+            spaces -= 2;
+        }else {
+            spaces += 2;
+        }
+
+    }
+}
 
 void print21(int n){
     int initial = 0;
@@ -243,7 +271,7 @@ int main(){
 for(int i = 0; i < t; i++){
     int n;
     cin >> n;
-    print19(n);
+    print20(n);
    
 
 }
